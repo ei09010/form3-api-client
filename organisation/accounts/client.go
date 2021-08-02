ccf7e449c1775bb5204da5cc9c5644c7f0fc2668
Mário Jorge Pereira <mario24jsp@gmail.com>
2021-08-03 00:00:28 +0100

    started working on delete method

--- organisation/accounts/client.go
+++ organisation/accounts/client.go
@@ -211,6 +211,80 @@ func (c *Client) postRequest(accountData *AccountData) (*AccountData, error) {
 	}
 }
 
+func (c *Client) Delete(accountId uuid.UUID, version int) error {
+
+	var err error
+
+	c.BaseURL.Path = path.Join(c.BaseURL.Path, fmt.Sprintf("/%s", accountId.String()))
+
+	c.BaseURL, err = c.BaseURL.Parse(c.BaseURL.Path)
+
+	if err != nil {
+
+		clientErr := handleClientError(FinalUrlParsingError, err.Error())
+
+		return clientErr
+	}
+
+	return c.deleteRequest()
+
+}
+
+func (c *Client) deleteRequest() error {
+
+	customReq, err := http.NewRequest("DELETE", c.BaseURL.String(), nil)
+
+	if err != nil {
+
+		return handleClientError(BuildingRequestError, err.Error())
+	}
+
+	// review headers
+	customReq.Header.Set("content-encoding", "application/json")
+	customReq.Header.Set("user-agent", "golang-sdk")
+
+	httpResponse, err := c.HttpClient.Do(customReq)
+
+	if err != nil {
+
+		return handleClientError(ExecutingRequestError, err.Error())
+	}
+
+	defer httpResponse.Body.Close()
+
+	if httpResponse != nil {
+
+		responseBody, err := ioutil.ReadAll(httpResponse.Body)
+
+		if err != nil {
+			return handleClientError(ResponseReadError, err.Error())
+		}
+
+		httpResponse.Body.Close()
+
+		if httpResponse.StatusCode == http.StatusOK {
+
+			return nil
+
+		} else {
+
+			apiHttpError := &ApiHttpError{}
+
+			err = json.Unmarshal(responseBody, apiHttpError)
+
+			if err != nil {
+				return handleClientError(UnmarshallingError, err.Error())
+			}
+
+			return handleBadStatusError(httpResponse.StatusCode, apiHttpError.ErrorMessage, httpResponse.Request.URL.Path)
+		}
+
+	} else {
+
+		return handleClientError(ResponseReadError, err.Error())
+	}
+}
+
 func handleClientError(errorType int, err string) *ClientError {
 
 	clientErr := &ClientError{
