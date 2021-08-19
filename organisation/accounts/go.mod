9bdf6f32e224643ffa01242826e756c8e010b3dd
mario <mario24jsp@gmail.com>
2021-08-19 22:43:25 +0100

    issue creating jsonb, they are a slice

--- organisation/accounts/go.mod
+++ organisation/accounts/go.mod
@@ -4,8 +4,7 @@ go 1.16
 
 require (
 	github.com/google/uuid v1.3.0
-	github.com/jinzhu/gorm v1.9.16 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
-	gorm.io/driver/postgres v1.1.0
-	gorm.io/gorm v1.21.13
+	github.com/jinzhu/gorm v1.9.16
+	github.com/lib/pq v1.3.0
+	github.com/stretchr/testify v1.7.0
 )
--- organisation/accounts/model.go
+++ organisation/accounts/model.go
@@ -2,8 +2,6 @@ package accounts
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 // Response containing all the information related to accounts
@@ -18,14 +16,11 @@ type AccountData struct {
 }
 
 type Account struct {
-	// gorm.Model `gorm:"type:Account"`
-	gorm.Model
 	Data  *Data  `json:"data" gorm:"type:data"`
 	Links *Links `json:"links" gorm:"type:links"`
 }
 
 type Data struct {
-	gorm.Model
 	Attributes     *AccountAttributes `json:"attributes" gorm:"type:attributes"`
 	CreatedOn      time.Time          `json:"created_on" gorm:"type:created_on"`
 	ID             string             `json:"id" gorm:"type:id"`
@@ -36,9 +31,8 @@ type Data struct {
 }
 
 type AccountAttributes struct {
-	gorm.Model
 	AccountClassification string   `json:"account_classification" gorm:"type:account_classification"`
-	AlternativeNames      []string `json:"alternative_names" gorm:"type:alternative_names"`
+	AlternativeNames      []string `json:"alternative_bank_account_names" gorm:"type:alternative_names"`
 	BankID                string   `json:"bank_id" gorm:"type:bank_id"`
 	BankIDCode            string   `json:"bank_id_code" gorm:"type:bank_id_code"`
 	BaseCurrency          string   `json:"base_currency" gorm:"type:base_currency"`
