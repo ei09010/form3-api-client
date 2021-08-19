357ecd36b98cd93b56a09d87a44b35a01ebfecfb
mario <mario24jsp@gmail.com>
2021-08-19 01:25:33 +0100

    working on the integration tests

--- organisation/accounts/go.mod
+++ organisation/accounts/go.mod
@@ -2,4 +2,10 @@ module ei09010/form3-api-client/organisation/accounts
 
 go 1.16
 
-require github.com/google/uuid v1.3.0 
+require (
+	github.com/google/uuid v1.3.0
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	gorm.io/driver/postgres v1.1.0
+	gorm.io/gorm v1.21.13
+)
--- organisation/accounts/model.go
+++ organisation/accounts/model.go
@@ -1,6 +1,10 @@
 package accounts
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 // Response containing all the information related to accounts
 type AccountResponse struct {
@@ -9,31 +13,40 @@ type AccountResponse struct {
 }
 
 type AccountData struct {
-	Data  *Data  `json:"data"`
-	Links *Links `json:"links"`
+	Data  *Data  `json:"data" gorm:"type:data"`
+	Links *Links `json:"links" gorm:"type:links"`
+}
+
+type Account struct {
+	// gorm.Model `gorm:"type:Account"`
+	gorm.Model
+	Data  *Data  `json:"data" gorm:"type:data"`
+	Links *Links `json:"links" gorm:"type:links"`
 }
 
 type Data struct {
-	Attributes     *AccountAttributes `json:"attributes"`
-	CreatedOn      time.Time          `json:"created_on"`
-	ID             string             `json:"id"`
-	ModifiedOn     time.Time          `json:"modified_on"`
-	OrganisationID string             `json:"organisation_id"`
-	Type           string             `json:"type"`
-	Version        int                `json:"version"`
+	gorm.Model
+	Attributes     *AccountAttributes `json:"attributes" gorm:"type:attributes"`
+	CreatedOn      time.Time          `json:"created_on" gorm:"type:created_on"`
+	ID             string             `json:"id" gorm:"type:id"`
+	ModifiedOn     time.Time          `json:"modified_on" gorm:"type:modified_on"`
+	OrganisationID string             `json:"organisation_id" gorm:"type:organisation_id"`
+	Type           string             `json:"type" gorm:"type:type"`
+	Version        int                `json:"version" gorm:"type:version"`
 }
 
 type AccountAttributes struct {
-	AccountClassification string   `json:"account_classification"`
-	AlternativeNames      []string `json:"alternative_names"`
-	BankID                string   `json:"bank_id"`
-	BankIDCode            string   `json:"bank_id_code"`
-	BaseCurrency          string   `json:"base_currency"`
-	Bic                   string   `json:"bic"`
-	Country               string   `json:"country"`
-	Name                  []string `json:"name"`
+	gorm.Model
+	AccountClassification string   `json:"account_classification" gorm:"type:account_classification"`
+	AlternativeNames      []string `json:"alternative_names" gorm:"type:alternative_names"`
+	BankID                string   `json:"bank_id" gorm:"type:bank_id"`
+	BankIDCode            string   `json:"bank_id_code" gorm:"type:bank_id_code"`
+	BaseCurrency          string   `json:"base_currency" gorm:"type:base_currency"`
+	Bic                   string   `json:"bic" gorm:"type:bic"`
+	Country               string   `json:"country" gorm:"type:country"`
+	Name                  []string `json:"name" gorm:"type:name"`
 }
 
 type Links struct {
-	Self string `json:"self"`
+	Self string `json:"self" gorm:"type:self"`
 }
